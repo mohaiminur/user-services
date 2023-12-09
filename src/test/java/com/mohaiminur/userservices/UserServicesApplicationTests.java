9dfe115ae4b16ce816047969a537b0404b2f2f04
Mohaiminur <mohaiminur404@gmail.com>
2023-12-09 20:14:44 +0600

    some update

--- src/test/java/com/mohaiminur/userservices/UserServicesApplicationTests.java
+++ src/test/java/com/mohaiminur/userservices/UserServicesApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -22,6 +23,69 @@ class UserServicesApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllUsers() {
+        List<User> userList = new ArrayList<>();
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> result = userService.getAllUsers();
+
+        assertEquals(userList.size(), result.size());
+    }
+
+    @Test
+    void getUserById() {
+        Long userId = 1L;
+        User sampleUser = new User(userId, "John", "Doe", "PARENT", 1L, null);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(sampleUser));
+
+        User result = userService.getUserById(userId);
+
+        assertEquals(sampleUser.getId(), result.getId());
+        assertEquals(sampleUser.getFirstName(), result.getFirstName());
+    }
+
+    @Test
+    void createUser() {
+        User newUser = new User(null, "Alice", "Smith", "CHILD", 1L, null);
+
+        userService.createUser(newUser);
+
+        verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    void updateUser() {
+        Long userId = 1L;
+        User updatedUser = new User(userId, "UpdatedFirstName", "UpdatedLastName", "PARENT", 1L, null);
+
+        userService.updateUser(updatedUser);
+
+        verify(userRepository, times(1)).save(updatedUser);
+    }
+
+    @Test
+    void deleteUser() {
+        Long userId = 1L;
+
+        userService.deleteUser(userId);
+
+        verify(userRepository, times(1)).deleteById(userId);
     }
 
 
